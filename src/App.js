e483c9bcc0d6901096fac286f184e73aa6ca9fd3
Nick Welch <nwelchr@gmail.com>
2023-08-28 17:46:52 -0700

    Add store + cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
-import { useState } from "react";
 import "./App.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
 import FormContainer from "./components/formContainer";
 
 function App() {
   return (
-    <div className="app">
-      <FormContainer />
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <FormContainer />
+      </div>
+    </Provider>
   );
 }
 
